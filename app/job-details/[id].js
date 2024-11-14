c92ea80dea3a19f97f686a48c9650468ff643912
okhi3945 <yhoka@naver.com>
2024-11-14 13:37:15 +0900

    11/14 RefreshControl 추가
    11/14 RefreshControl 추가

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,7 +1,11 @@
 import { Stack, useRouter, useLocalSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
 import useFetch from '../../hook/useFetch';
-import { View, Text, ActivityIndicator, Image, ScrollView, Linking, StyleSheet } from 'react-native';
+import { View, Text, ActivityIndicator, Image, ScrollView, RefreshControl, SafeAreaView } from 'react-native';
+import { COLORS, icons, SIZES } from '../../constants' // 디자인에 필요한 요소들 import
+import { ScreenHeaderBtn } from '../components'
 
+//  RefreshControl : refetch 해줄 때 사용 -> 서버가 값을 잘 못가져올 때 새로고침 기능 
 /* 
     useLocalSearchParams → [id].js → 동적 라우팅 → 
     url의 특정 부분을 변수로 사용하여 다양한 페이지를 생성 → 
@@ -11,152 +15,55 @@ import { View, Text, ActivityIndicator, Image, ScrollView, Linking, StyleSheet }
 
 const JobDetails = () => {
     const params = useLocalSearchParams();
-    const { data, isLoading, error } = useFetch(
+    const router = useRouter();
+    const { data, isLoading, error, refetch } = useFetch( // 새로고침을 위한 refetch를 useFetch()로 부터 가져옴
         "job-details", {
         job_id: params.id,
     }
     );
     console.log(data)
-    if (isLoading) {
-        return (
-            <View style={styles.loadingContainer}>
-                <ActivityIndicator size="large" color="#0000ff" />
-                <Text>로딩 중...</Text>
-            </View>
-        );
-    }
 
-    if (error) {
-        return (
-            <View style={styles.errorContainer}>
-                <Text>오류 발생: {error.message}</Text>
-            </View>
-        );
-    }
+    const [refreshing, setRefreshing] = useState(false) // refreshing 상태를 사용하여 새로 고침 중인지 여부를 나타냄
+    const onRefresh = useCallback(() => { // 리렌더링 시 동일한 함수를 재사용하도록 함
+        setRefreshing(true)
+        refetch()
+        setRefreshing(false)
+    })
 
-    if (!data || data.length === 0) {
-        return (
-            <View style={styles.errorContainer}>
-                <Text>데이터가 없습니다.</Text>
-            </View>
-        );
-    }
-
-    const job = data[0];
 
     return (
-        <ScrollView style={styles.container}>
-            <Image source={{ uri: job.employer_logo }} style={styles.logo} />
-            <Text style={styles.jobTitle}>{job.job_title || "직무 제목 없음"}</Text>
+        <SafeAreaView>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerBackVisible: false,
+                    headerTitle: "",
+                    headerLeft: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.left}
+                            dimension="60%"
+                            handlePress={() => router.back()}
+                        />
+                    ), // ()=>() 안에 최상위 태그 사용
+                    headerRight: () => (
+                        <ScreenHeaderBtn
+                            iconUrl={icons.share}
+                            dimension={"60%"}
+                        />
+                    )
+                }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false} // 스크롤바 false
+                refreshControl={<RefreshControl
+                    refreshing={refreshing} // 스크롤뷰에서 스와이프하여 새로고침 기능을 추가, 아래로 스와이프시 onRefresh() 호출되어 데이터 새로 가져옴
+                    onRefresh={onRefresh}
+                />} // 값 새로고침을 위해 RefreshControl 사용
+            >
 
-            <View style={styles.table}>
-                <View style={styles.tableHeader}>
-                    <Text style={styles.headerTextLeft}>정보</Text>
-                    <Text style={styles.headerText}>내용</Text>
-                </View>
-                <View style={styles.tableRow}>
-                    <Text style={styles.rowLabel}>고용주 이름</Text>
-                    <Text style={styles.rowValue}>{job.employer_name}</Text>
-                </View>
-                <View style={styles.tableRow}>
-                    <Text style={styles.rowLabel}>위치</Text>
-                    <Text style={styles.rowValue}>{job.job_city}, {job.job_state}, {job.job_country}</Text>
-                </View>
-                <View style={styles.tableRow}>
-                    <Text style={styles.rowLabel}>고용 형태</Text>
-                    <Text style={styles.rowValue}>{job.job_employment_type}</Text>
-                </View>
-                <View style={styles.tableRow}>
-                    <Text style={styles.rowLabel}>필요한 기술</Text>
-                    <Text style={styles.rowValue}> 
-                        {job.job_required_skills && job.job_required_skills.length > 0 // job_required_skills가 null이거나, length가 0보다 같거나 작으면 없음이라고 표시
-                            ? job.job_required_skills.join(', ')
-                            : "없음"}
-                    </Text>
-                </View>
-                <View style={styles.tableRow}>
-                    <Text style={styles.rowLabel}>직무 설명</Text>
-                    <Text style={styles.rowValue}>{job.job_description}</Text>
-                </View>
-                <View style={styles.tableRow}>
-                    <Text style={styles.rowLabel}>지원 링크</Text>
-                    <Text
-                        style={styles.rowValueLink}
-                        onPress={() => Linking.openURL(job.job_apply_link)}
-                    >
-                        {job.job_apply_link}
-                    </Text>
-                </View>
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        padding: 20,
-        backgroundColor: '#fff',
-    },
-    loadingContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    errorContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    logo: {
-        width: 100,
-        height: 100,
-        alignSelf: 'center',
-        marginBottom: 20,
-    },
-    jobTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 20,
-    },
-    table: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 8,
-        overflow: 'hidden',
-    },
-    tableHeader: {
-        backgroundColor: '#f2f2f2',
-        flexDirection: 'row',
-        padding: 10,
-    },
-    headerText: {
-        flex: 1,
-        fontWeight: 'bold',
-        textAlign: 'left',
-    }, headerTextLeft: {
-        flex: 1,
-        fontWeight: 'bold',
-        marginLeft: '1%'
-    },
-    tableRow: {
-        flexDirection: 'row',
-        padding: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-    },
-    rowLabel: {
-        flex: 1,
-        fontWeight: 'bold',
-    },
-    rowValue: {
-        flex: 2,
-    },
-    rowValueLink: {
-        flex: 2,
-        color: 'blue',
-        textDecorationLine: 'underline',
-    },
-});
-
-export default JobDetails;
+export default JobDetails
